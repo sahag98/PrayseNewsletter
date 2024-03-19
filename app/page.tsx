5d0417b0f8133f9e098803f90edcc5399037f5b6
Sahak Arzoumanian <arzsahag@gmail.com>
2024-03-18 17:02:45 -0700

    added text under heading

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,19 @@ export default function Home() {
             alt="Prayse Logo"
           />
           <div>
-            <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
+            <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-500 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
               Join our
             </h2>
 
             <h1
-              className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl 
+              className="z-10 text-4xl font-bold text-center text-transparent duration-500 bg-white cursor-default sm:text-6xl md:text-7xl 
         whitespace-nowrap bg-clip-text bg-gradient-to-r from-indigo-500 to-blue-500 animate-fade-in-3"
             >
-              Prayse Newsletter
+              Newsletter
             </h1>
+            <p className="z-10  font-bold text-center text-transparent duration-800 animate-fade-in-3 bg-white/60 cursor-default   whitespace-nowrap bg-clip-text">
+              And stay up-to-date with our most recent updates&news!
+            </p>
           </div>
         </div>
         <NewsletterForm />
